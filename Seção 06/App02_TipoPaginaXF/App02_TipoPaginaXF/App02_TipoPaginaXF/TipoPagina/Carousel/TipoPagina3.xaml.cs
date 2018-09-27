68e5fbd3056c0d83de47339f4cfb939551008300
Francelio Alencar <francelio.si@gmail.com>
2018-09-27 19:02:16 -0300

    app 2 seção 6

---
+++ Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Carousel/TipoPagina3.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF.TipoPagina.Carousel
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TipoPagina3 : ContentPage
+	{
+		public TipoPagina3()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarPagina(object sender, EventArgs e)
+        {   //sem navegation
+            //App.Current.MainPage = new Navigation.Pagina1();
+            //com navegation para pagina 1 
+            //App.Current.MainPage = new NavigationPage(new Navigation.Pagina1()) { BarBackgroundColor=Color.Bisque};
+            //com navegation para ABAS 
+            App.Current.MainPage = new Tabbed.Abas();
+        }
+    }
+}
