055ae8df6a104a131026f75c2d376dcab3047c41
Francelio Alencar <francelio.si@gmail.com>
2018-10-03 23:54:10 -0300

    app01_Layouts

---
+++ Seção 07/App01_LayoutsXF/App01_LayoutsXF/App01_LayoutsXF/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_LayoutsXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void GoPaginasAbsolute(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+        }
+        private void GoPaginasGrid(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Grid.GridPage());
+        }
+        private void GoPaginasRelative(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Relative.RelativePage());
+        }
+        private void GoPaginasScroll(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+        }
+        private void GoPaginasStack(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Stack.StackPage());
+        }
+    }
+}
