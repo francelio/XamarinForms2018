67c755067a1b41eeba99f17c60ce29819e7f6ac3
Francelio Alencar <francelio.si@gmail.com>
2018-10-04 23:58:43 -0300

    Projeto App Layouts

---
+++ Seção 07/App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_Xamarin.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+    {
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoPaginaPerfil1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Perfil1());
+            IsPresented = false;
+           
+        }
+        private void GoPaginaPerfil2(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Perfil2());
+            IsPresented = false;
+           
+        }
+        private void GoPaginaPerfil3(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Perfil3());
+            IsPresented = false;
+        }
+        private void GoPaginaXamarin(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Xamarin());
+            IsPresented = false;
+        }
+    }
+}
