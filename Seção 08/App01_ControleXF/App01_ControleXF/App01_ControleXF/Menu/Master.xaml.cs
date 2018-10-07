8cf418b2ab65b3135296b8117dc01782c3434d79
Francelio Alencar <francelio.si@gmail.com>
2018-10-06 22:01:04 -0300

    correções IOS

--- Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
+++ Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
@@ -9,14 +9,13 @@ using Xamarin.Forms.Xaml;
 
 namespace App01_ControleXF.Menu
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Master : MasterDetailPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
     {
-		public Master ()
-		{
-			InitializeComponent ();
-		}
-
+        public Master()
+        {
+            InitializeComponent();
+        }
         private void GoActivityIndicatorPage(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Controles.ActivityIndicatorPage());
@@ -97,6 +96,6 @@ namespace App01_ControleXF.Menu
             Detail = new NavigationPage(new Controles.WebViewPage());
             IsPresented = false;
         }
-           
+
     }
 }
